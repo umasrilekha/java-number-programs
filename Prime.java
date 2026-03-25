322160dcb13c307ad6353f6439ba4c9e110d0902
Jandhyala Uma Sri Lekha <umasrilekhaj@gmail.com>
2026-03-25 21:00:12 +0530

    Adding file Prime.java

---
+++ Prime.java
@@ -0,0 +1,14 @@
+public class Prime {
+	public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+	public static void main(String[] args) {
+		System.out.println(isPrime(9));
+	}
+}
